17cc169c59a5891ad03ce42474940cd9c118d30d
Author <1084777253@qq.com>
2021-05-19 10:30:28 +0800

    Hello World

---
+++ 0.0/0_0.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: Author
+ * @Date: 2021-05-19 10:26:41
+ * @LastEditors: Author
+ * @LastEditTime: 2021-05-19 10:27:08
+ * @FilePath: \C_PrimerPlus\0.0\0_0.c
+ */
+#include  <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
